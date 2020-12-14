703989f597bcc02fb82a8fd43e58fe5fad653cab
hungphandinh <hungphandinh.dev@gmail.com>
2020-12-14 15:59:24 +0700

    Change to python2

--- ckan-base/2.9py2/setup/start_ckan.sh
+++ ckan-base/2.9py2/setup/start_ckan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the prerun script to init CKAN and create the default admin user
-sudo -u ckan -EH python3 prerun.py
+sudo -u ckan -EH python prerun.py
 
 # Run any startup scripts provided by images extending this one
 if [[ -d "/docker-entrypoint.d" ]]
@@ -9,7 +9,7 @@ then
     for f in /docker-entrypoint.d/*; do
         case "$f" in
             *.sh)     echo "$0: Running init file $f"; . "$f" ;;
-            *.py)     echo "$0: Running init file $f"; python3 "$f"; echo ;;
+            *.py)     echo "$0: Running init file $f"; python "$f"; echo ;;
             *)        echo "$0: Ignoring $f (not an sh or py file)" ;;
         esac
         echo
